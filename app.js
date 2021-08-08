e78cbbf575171ad7c37b4de30fddb222d73fe098
Emerald Hines <emeraldhines43@gmail.com>
2021-08-07 21:05:42 -0500

    fixed problems with code when running node

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+//var message = 'Hello Node!';                       
+//var sum = 5+3;
+//console.log(message);
+//console.log(sum);
 
+//var commandLineArgs = process.argv;
+///console.log(commandLineArgs);
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
@@ -6,4 +12,10 @@ console.log(profileDataArgs);
 //Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
     for (let i=0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
+        console.log(profileDataArr[i])
+        };
+
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileitem)
+    });
+};
